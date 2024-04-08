bb6af3bce5cb1973f5ab9e9f8ab5feab648e9c90
nursultanaid <atakexs@gmail.com>
2024-04-08 23:36:25 +0600

    sixth commit

--- product/views.py
+++ product/views.py
@@ -1,25 +1,91 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import HttpResponse
 from datetime import datetime
+from django.db.models import Q
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView
 
 from product.forms import ProductForm, ProductForm2, ReviewForm
-from product.models import Product, Category, Review
+from product.models import Product, Category, Review, Tag
 
+
+class HelloView(View):
+    def get(self,request):
+        return HttpResponse("Hello! Its my project")
 
 def hello_view(request):
     return HttpResponse("Hello! Its my project")
 
 current_date = datetime.now()
+
+class CurrentDateView(View):
+    def get(self,request):
+        return HttpResponse(current_date)
+
 def current_data_view(request):
     return HttpResponse(current_date)
 
+class GoodbyeView(View):
+    def get(self,request):
+        return HttpResponse("Goodbye user!")
+
 def goodbye_view(request):
     return HttpResponse("Goodbye user!")
+
+class MainView(View):
+    def get(self,request):
+        context = {'name': 'Nursultan'}
+        return render(request, 'main.html', context=context)
+
 
 def main_view(request):
     context = {'name': 'Nursultan'}
     if request.method == "GET":
         return render(request, 'main.html', context=context)
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product/product_list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        search = self.request.GET.get('search')
+        sort = self.request.GET.get('sort', 'created_at')
+        tag = self.request.GET.get('tag')
+        page = self.request.GET.get('page', 1)
+
+        posts = Product.objects.all()
+
+        start = (int(page) - 1) * 3
+        end = int(page) * 3
+
+        if search:
+            posts = posts.filter(
+                Q(title__icontains=search) | Q(content__icontains=search)
+            )
+
+        if tag:
+            posts = posts.filter(tags__id=tag)
+
+        return posts.order_by(sort)[start:end]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
+        posts = Product.objects.all()
+        limit = 3
+
+        all_pages = len(posts) / limit
+
+        if round(all_pages) < all_pages:
+            all_pages += 1
+        all_pages = round(all_pages)
+
+        context['all_pages'] = range(1, all_pages + 1)
+        return context
+
 
 def product_list_view(request):
     products = Product.objects.all()
@@ -30,6 +96,13 @@ def product_list_view(request):
 
     return render(request, 'product/product_list.html', context)
 
+class ProductDetailView(View):
+    def get(self, request, product_id):
+        product = Product.objects.get(id=product_id)
+
+        context = {'product': product}
+        return render(request, 'product/product_detail.html', context)
+
 def product_detail_view(request, product_id):
     try:
         product = Product.objects.get(id=product_id)
@@ -38,10 +111,28 @@ def product_detail_view(request, product_id):
     context = {'product': product}
     return render(request, 'product/product_detail.html', context)
 
+class CategoryView(View):
+    def get(self, request):
+        category = Category.objects.all()
+        context = {'category': category}
+        return render(request, 'product/category.html', context)
+
 def category_view(request):
     category = Category.objects.all()
     context = {'category': category}
     return render(request, 'product/category.html', context)
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm2
+    template_name = 'products/products_create.html'
+    success_url = '/products/'
+
+    def get_absolute_url(self):
+        if self.request.user.is_authenticated:
+            return reverse('product_list')
+        return reverse('login')
+
 
 def product_create_view(request):
     if request.method == 'GET':
@@ -65,6 +156,23 @@ def product_create_view(request):
         product.tags.set(tags)
         product.save()
         return redirect('/products/')
+
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, 'product/review.html', {'form': form})
+
+    def post(self, request):
+        form = ReviewForm(request.POST)
+
+        if not form.is_valid():
+            return render(request, 'product/review.html', {'form': form})
+        text = form.cleaned_data['text']
+
+        review = Review.objects.create(text=text)
+        review.save()
+        return redirect('/products/<int:product_id>/')
+
 
 def review_view(request):
     if request.method == 'GET':
--- shop/urls.py
+++ shop/urls.py
@@ -2,22 +2,44 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from product.views import (main_view, hello_view, current_data_view, goodbye_view,
-                           product_list_view, product_detail_view, category_view,
-                           product_create_view, review_view)#product_change_view, product_delete_view)
-
-# from user.views import register_view, login_view, logout_view
+from product.views import (main_view, hello_view, current_data_view, goodbye_view,product_list_view,
+                           product_detail_view, category_view, product_create_view, review_view,
+                           HelloView, CurrentDateView, GoodbyeView, MainView, ProductListView,
+                           ProductDetailView, CategoryView, ProductCreateView, ReviewView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', main_view, name='main'),
+    path('main2/', MainView.as_view(), name='main2'),
+
     path('current_data/', current_data_view, name='current_data'),
+    path('current_data2/', CurrentDateView.as_view(), name='current_data2'),
+
     path('goodbye/', goodbye_view, name='goodbye'),
+    path('goodbye2/', GoodbyeView.as_view(), name='goodbye2'),
+
     path('hello/', hello_view, name='hello'),
+    path('hello2/', HelloView.as_view(), name='hello2'),
+
     path('products/', product_list_view, name='product_list'),
+    path('products2/', ProductListView.as_view(), name='product_list2'),
+
+
     path('products/<int:product_id>', product_detail_view, name='product_detail'),
+    path('products/<int:product_id>.2/', ProductDetailView.as_view(), name='product_detail2'),
+
+
     path('category/', category_view, name='category'),
+    path('category2/', CategoryView.as_view(), name='category2'),
+
+
     path('product_create/', product_create_view, name='product_create'),
-    path('reviews/', review_view, name='reviews')
+    path('product_create2/', ProductCreateView.as_view(), name='product_create2'),
+
+
+    path('reviews/', review_view, name='reviews'),
+    path('reviews2/', ReviewView.as_view(), name='reviews2')
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
